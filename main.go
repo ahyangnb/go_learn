4a1f0904d1e172a8795b3fcc89fb32e9b38adb2e
ahyangnb <zonggeu@gmail.com>
2020-09-13 19:12:17 +0800

    commit new ip

--- main.go
+++ main.go
@@ -13,7 +13,8 @@ func main() {
 	http.HandleFunc("/h1", h1)
 	http.HandleFunc("/test", testHandle)
 	//服务器要监听的主机地址和端口号
-	http.ListenAndServe("127.0.0.1:8081", nil)
+	http.ListenAndServe("42.51.67.29:8888", nil)
+	//http.ListenAndServe("127.0.0.1:8081", nil)
 }
 
 type Data struct {
