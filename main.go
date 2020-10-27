48611777bcc1135cbb648d2297b8912c71a326c9
ahyangnb <zonggeu@gmail.com>
2020-10-27 11:49:07 +0800

    commit hello.tmpl

--- main.go
+++ main.go
@@ -3,32 +3,34 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"go_learn/src/server_text"
 	"go_learn/src/type_def"
 	"io"
 	"net/http"
 )
 
 func main() {
+	server_text.TemplateText()
 	//demo_digui.DemoDiGui()
 	//demo_error.ErrorHandle()
 	//demo_type.TimeTest()
 	//demo_file.BufioReadFile()
 	//server_text.TemplateText()
 	//////第一个参数是接口名，第二个参数 http handle demo_func
-	http.HandleFunc("/h1", h1)
-	http.HandleFunc("/test", testHandle)
-	////服务器要监听的主机地址和端口号
-	fmt.Println("===================")
-	fmt.Println("welcome to my serve")
-	fmt.Println("===================")
-	host := "0.0.0.0:3389"
-	//host := "127.0.0.1:3389"
-	//host := "81.71.139.64:3389"
-	err := http.ListenAndServe(host, nil)
-	if err != nil {
-		fmt.Printf("%v", err)
-	}
-	fmt.Printf("listen tcp %v 成功\n", host)
+	//http.HandleFunc("/h1", h1)
+	//http.HandleFunc("/test", testHandle)
+	//////服务器要监听的主机地址和端口号
+	//fmt.Println("===================")
+	//fmt.Println("welcome to my serve")
+	//fmt.Println("===================")
+	//host := "0.0.0.0:3389"
+	////host := "127.0.0.1:3389"
+	////host := "81.71.139.64:3389"
+	//err := http.ListenAndServe(host, nil)
+	//if err != nil {
+	//	fmt.Printf("%v", err)
+	//}
+	//fmt.Printf("listen tcp %v 成功\n", host)
 
 }
 
--- src/server_text/template_text.go
+++ src/server_text/template_text.go
@@ -1,5 +1,30 @@
 package server_text
 
-func TemplateText() {
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
 
+func TemplateText() {
+	main()
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	// 解析指定文件生成模板对象
+	tmpl, err := template.ParseFiles("./hello.tmpl")
+	if err != nil {
+		fmt.Println("create template failed, err:", err)
+		return
+	}
+	// 利用给定数据渲染模板，并将结果写入w
+	tmpl.Execute(w, "沙河小王子")
+}
+func main() {
+	http.HandleFunc("/", sayHello)
+	err := http.ListenAndServe(":3389", nil)
+	if err != nil {
+		fmt.Println("HTTP server failed,err:", err)
+		return
+	}
 }
