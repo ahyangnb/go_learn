98c6fd5ea59794b2eb7340900c1dde52426e39a9
ahyangnb <zonggeu@gmail.com>
2020-10-27 16:29:58 +0800

    commit insertRowDemo

--- src/demo_sql/demo_sql.go
+++ src/demo_sql/demo_sql.go
@@ -36,12 +36,30 @@ func SqlTest() {
 		return
 	}
 	queryRowDemo()
+	//queryMultiRowDemo()
+	//insertRowDemo()
 }
 
 type user struct {
 	id   int
 	age  int
 	name string
+}
+
+// 插入数据
+func insertRowDemo() {
+	sqlStr := "insert into user(name, age) values (?,?)"
+	ret, err := db.Exec(sqlStr, "王五", 38)
+	if err != nil {
+		fmt.Printf("insert failed, err:%v\n", err)
+		return
+	}
+	theID, err := ret.LastInsertId() // 新插入数据的id
+	if err != nil {
+		fmt.Printf("get lastinsert ID failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("insert success, the id is %d.\n", theID)
 }
 
 // 查询单条数据示例
@@ -55,4 +73,27 @@ func queryRowDemo() {
 		return
 	}
 	fmt.Printf("id:%d name:%s age:%d\n", u.id, u.name, u.age)
+}
+
+// 查询多条数据示例
+func queryMultiRowDemo() {
+	sqlStr := "select id, name, age from user where id > ?"
+	rows, err := db.Query(sqlStr, 0)
+	if err != nil {
+		fmt.Printf("query failed, err:%v\n", err)
+		return
+	}
+	// 非常重要：关闭rows释放持有的数据库链接
+	defer rows.Close()
+
+	// 循环读取结果集中的数据
+	for rows.Next() {
+		var u user
+		err := rows.Scan(&u.id, &u.name, &u.age)
+		if err != nil {
+			fmt.Printf("scan failed, err:%v\n", err)
+			return
+		}
+		fmt.Printf("id:%d name:%s age:%d\n", u.id, u.name, u.age)
+	}
 }
