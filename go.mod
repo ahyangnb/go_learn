75ea92cc9349a189313e73536ac29dcaff04e98d
ahyangnb <zonggeu@gmail.com>
2020-11-25 11:54:34 +0800

    commit redis connect

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module go_learn
 
 go 1.14
 
-require github.com/go-sql-driver/mysql v1.5.0
+require (
+	github.com/astaxie/beego v1.12.3
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/go-sql-driver/mysql v1.5.0
+)
---
+++ src/demo_redis/demo_redis.go
@@ -0,0 +1,33 @@
+package demo_redis
+
+import (
+	"fmt"
+	"github.com/go-redis/redis"
+)
+
+// 声明一个全局的rdb变量
+var rdb *redis.Client
+
+// 初始化连接
+func initClient() (err error) {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	_, err = rdb.Ping().Result()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DemoRedis() {
+	err := initClient()
+	if err != nil {
+		fmt.Print(err)
+	} else {
+		fmt.Print("连接成功")
+	}
+}
