030e233d37dd4bd0af7d435a98e7fe58ef2927c1
ahyangnb <zonggeu@gmail.com>
2020-11-25 12:06:05 +0800

    commit V8Example

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module go_learn
 go 1.14
 
 require (
-	github.com/astaxie/beego v1.12.3
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/go-redis/redis/v8 v8.4.0
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 )
--- src/demo_redis/demo_redis.go
+++ src/demo_redis/demo_redis.go
@@ -1,33 +1,81 @@
 package demo_redis
 
 import (
+	"context"
 	"fmt"
-	"github.com/go-redis/redis"
+	"github.com/go-redis/redis/v8"
+	"time"
 )
 
 // 声明一个全局的rdb变量
 var rdb *redis.Client
 
 // 初始化连接
-func initClient() (err error) {
-	rdb = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
-
-	_, err = rdb.Ping().Result()
-	if err != nil {
-		return err
-	}
-	return nil
-}
+//func initClient() (err error) {
+//	rdb = redis.NewClient(&redis.Options{
+//		Addr:     "localhost:6379",
+//		Password: "", // no password set
+//		DB:       0,  // use default DB
+//	})
+//
+//	_, err = rdb.Ping().Result()
+//	if err != nil {
+//		return err
+//	}
+//	return nil
+//}
 
 func DemoRedis() {
-	err := initClient()
+	V8Example()
+	//err := initClientV8()
+	//if err != nil {
+	//	fmt.Printf("连接失败::%v", err)
+	//} else {
+	//	fmt.Print("连接成功")
+	//}
+}
+
+// 初始化连接
+func initClientV8() (err error) {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",  // no password set
+		DB:       0,   // use default DB
+		PoolSize: 100, // 连接池大小
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err = rdb.Ping(ctx).Result()
+	return err
+}
+
+func V8Example() {
+	ctx := context.Background()
+	if err := initClientV8(); err != nil {
+		return
+	}
+
+	err := rdb.Set(ctx, "key", "value", 0).Err()
 	if err != nil {
-		fmt.Printf("连接失败::%v", err)
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	_, err = rdb.Set(ctx, "key2", "我是Key2的值", 0).Result()
+
+	val2, err := rdb.Get(ctx, "key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
 	} else {
-		fmt.Print("连接成功")
+		fmt.Println("key2", val2)
 	}
 }
