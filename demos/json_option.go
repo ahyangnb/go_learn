434fd30933443a38f2f877b2d2252d97d6907ce6
ahyangnb <zonggeu@gmail.com>
2021-02-14 15:46:18 +0800

    commit json反序列化

--- demos/json_option.go
+++ demos/json_option.go
@@ -7,8 +7,8 @@ import (
 
 type testJson struct {
 	// json:"id"表示在json里key的名字，这里是写成小写开头的
-	Id   int64 `json:"id"`
-	Name string
+	Id   int64  `json:"id"`
+	Name string `json:"name"`
 }
 
 func main() {
@@ -20,8 +20,17 @@ func main() {
 		fmt.Printf("error::%v", err)
 	}
 	// 以string方式打印json
-	fmt.Printf("json序列化:%v", string(v))
+	fmt.Printf("json序列化:%v\n", string(v))
 
+	// 2.json反序列化
+	str := `{"id":11,"name":"哈哈哈"}`
+	var testJson1 testJson
+	err1 := json.Unmarshal([]byte(str), &testJson1)
+	if err1 != nil {
+		fmt.Printf("err1::%v", err1)
+	}
+	// 以string方式打印反序列化json
+	fmt.Printf("json反序列化:%v\n", testJson1)
 }
 
 func newTestJson(Id int64, Name string) testJson {
