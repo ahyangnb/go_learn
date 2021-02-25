8659cfb1d85c9ef3bd9cc84ca2ff2d675b217043
ahyangnb <zonggeu@gmail.com>
2021-02-26 00:20:31 +0800

    commit special

--- demos/interface.go
+++ demos/interface.go
@@ -26,10 +26,39 @@ type animal interface {
 	speak()
 }
 
+type returnValueType struct {
+}
+
+// 特殊的接口
+type returnValueInterFace interface {
+	// 有返回值的接口
+	returnValue() (age int, name string)
+	// 有参数的接口
+	paramValue(age int, name string)
+}
+
+func (returnValueType) returnValue() (age int, name string) {
+	fmt.Print("returnValue")
+	return 1, "哈哈"
+}
+
+func (returnValueType) paramValue(age int, name string) {
+	fmt.Printf("paramValue,,age:%v,name:%v", age, name)
+}
+
+func test(r returnValueInterFace) {
+	r.paramValue(1, "a")
+	r.returnValue()
+}
+
 func main() {
 	var d dog
 	da(d)
 
 	var c cat
 	da(c)
+
+	// 特殊
+	var r returnValueType
+	test(r)
 }
