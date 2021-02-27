f10ef9b2b307988ed0b2b9c014c3dedd6c40eb56
ahyangnb <zonggeu@gmail.com>
2021-02-27 10:33:56 +0800

    commit switch

--- demos/assert_demo.go
+++ demos/assert_demo.go
@@ -3,16 +3,21 @@ package main
 import "fmt"
 
 func assert(value interface{}) {
-	// 判断是否为string
-	str, strOk := value.(string)
-	if strOk {
-		fmt.Printf("猜对了，值是：%v\n", str)
-	} else {
+
+	switch value.(type) {
+	case string:
+		str := value.(string)
+		fmt.Printf("string，值是：%v\n", str)
+	case int:
+		intValue := value.(int)
+		fmt.Printf("int，值是：%v\n", intValue)
+	default:
 		fmt.Printf("猜错了\n")
 	}
 }
 
 func main() {
-	assert("哈哈哈") //对的
-	assert(1)     //错的
+	assert("哈哈哈") //对的[string]
+	assert(1)     //对的[int]
+	assert(1.1)   //错的
 }
