e611d17626237866bb81bce6c4da3a991f71efc1
q1 <zonggeu@gmail.com>
2021-06-15 11:47:39 +0800

    PostForm

--- demos/gin.go
+++ demos/gin.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func main() {
 	r := gin.Default()
+
+	// 设置模式
+	gin.SetMode(gin.ReleaseMode)
+
 	r.POST("/ping", func(c *gin.Context) {
+		message := c.PostForm("message")
+		fmt.Printf("message::%v\n", message)
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-
-	// 设置模式
-	gin.SetMode(gin.ReleaseMode)
 
 	err := r.Run()
 	if err != nil {
